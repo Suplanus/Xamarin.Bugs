31dcbe19ff112c55acc3e22ff1046882e2c086f9
Suplanus <info@suplanus.de>
2020-01-09 14:59:57 +0100

    Issue8981

--- src/Xamarin.Bugs/App.xaml.cs
+++ src/Xamarin.Bugs/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Xamarin.Bugs
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Issue8981();
         }
 
         protected override void OnStart ()
---
+++ src/Xamarin.Bugs/Issue8981.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin.Bugs
+{
+    public partial class Issue8981 : ContentPage
+    {
+        public Issue8981()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+        }
+    }
+}
