a784bc4a0f6b79e2d91e512c639d935bb9ae15fd
Suplanus <info@suplanus.de>
2020-01-09 14:56:48 +0100

    gitignore

---
+++ src/Xamarin.Bugs/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.Bugs
+{
+    [DesignTimeVisible(false)]
+    public partial class Issue8981 : ContentPage
+    {
+        public Issue8981()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            
+        }
+    }
+}
