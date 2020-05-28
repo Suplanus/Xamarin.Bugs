b15c2214d7a56c2be929ccb41f35f782918d52a7
Johann Weiher <info@suplanus.de>
2020-05-28 07:43:28 +0200

    10360

--- src/Xamarin.Bugs/AssemblyInfo.cs
+++ src/Xamarin.Bugs/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+
+[assembly: ExportFont("FontAwesome5Brands.otf", Alias = "FontAwesome5Brands")]
+[assembly: ExportFont("FontAwesome5Regular.otf", Alias = "FontAwesome5Regular")]
+[assembly: ExportFont("FontAwesome5Solid.otf", Alias = "FontAwesome5Solid")]
